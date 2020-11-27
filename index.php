1e9f84772d790077851ae8e429323825157975c0
diegodc26 <ti.diegooliveira@gmail.com>
2020-11-27 17:27:46 -0300

    master

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF -8>">
-    <title>Formulario de inscrição</title>
+    <title>Formulário de inscrição</title>
     <meta name="author" content="Diego">
     <meta name="description" content="form php">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,7 +11,7 @@
 
 <body>
 
-    <p>FORMULARIO PARA inscrição DE COMPETIDORES</p>
+    <p>FORMULÁRIO PARA inscrição DE COMPETIDORES</p>
 
     <form action="script.php" method="post">
         <p>Seu nome: <input type="text" name="nome" /></p>
