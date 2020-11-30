b0b2f7486c3f7b3c2d5864b10c54095e6e6e361f
diegodc26 <ti.diegooliveira@gmail.com>
2020-11-30 11:57:31 -0300

    Inserindo validacoes nos atributos

--- script.php
+++ script.php
@@ -12,6 +12,30 @@ $categorias[] = 'adulto';
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
+//validação nome
+if(empty($nome)){
+    echo 'O nome não pode estar em branco';
+    return;
+}
+if (strlen($nome) <=2){
+    echo 'O nome deve conter mais que 2 caracteres';
+    return;
+}elseif(strlen($nome) >=40){
+    echo 'Nome muito extenso';
+    return;
+}
+
+//validação idade
+if(empty($idade) and $idade <=0){
+    echo 'Idade não pode ser vazia';
+    return;
+}
+if(!is_numeric($idade)){
+    echo 'Idade tem que ser um numero';
+    return;
+}
+
+
 //var_dump($nome, $idade);
 
 if ($idade >= 6 && $idade <=12){
